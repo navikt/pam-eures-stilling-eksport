e9b6e9319e91f587d995d69e7f741a3bd9c4d985
Einar Bjerve <einar.bjerve@nav.no>
2019-12-03 14:41:50 +0100

    Should now handle country code mapping properly

--- src/main/kotlin/no/nav/pam/euresstillingeksport/model/pam/PositionLocationConverter.kt
+++ src/main/kotlin/no/nav/pam/euresstillingeksport/model/pam/PositionLocationConverter.kt
@@ -2,6 +2,7 @@ package no.nav.pam.euresstillingeksport.model.pam
 
 import no.nav.pam.euresstillingeksport.model.eures.Address
 import no.nav.pam.euresstillingeksport.model.eures.PositionLocation
+import no.nav.pam.geography.CountryDAO
 
 
 fun Location.toPositionLocation(): PositionLocation {
@@ -11,13 +12,17 @@ fun Location.toPositionLocation(): PositionLocation {
             countryCode = countryToCountryCode(country),
             countrySubDivisionCode = null,
             postalCode = postalCode
-
     ))
 }
 
-fun countryToCountryCode(country: String?): String { // TODO kodeverk-ting
-    return when(country){
-        "NORGE" -> "NO"
-        else -> "NO"
-    }
+private val countries: CountryDAO = CountryDAO()
+
+private fun countryToCountryCode(country: String?): String {
+
+    return countries.findCountry(country)
+            .map { it.code }
+            .map { if( it == "GB") "UK" else it } // Spesial-håndtering av Storbritannia i EUs kodeverk
+            .map { if( it == "GR") "EL" else it } // Spesial-håndtering av Hellas i EUs kodeverk
+            .orElse("NO") // TODO Burde filtrere bort stillinger utenfor EU
 }
+
