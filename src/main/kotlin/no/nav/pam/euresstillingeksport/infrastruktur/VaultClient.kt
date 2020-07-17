b0987d3c593f92b4850df93aab02dd9b89ad41eb
Tore Engvig <Tore.Engvig@nav.no>
2020-07-17 11:27:25 +0200

    ignore unknown på _alt_ som skal ha det...

--- src/main/kotlin/no/nav/pam/euresstillingeksport/infrastruktur/VaultClient.kt
+++ src/main/kotlin/no/nav/pam/euresstillingeksport/infrastruktur/VaultClient.kt
@@ -19,6 +19,7 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.time.Duration
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Credential(val username: String, val password: String, val ttl: Int)
 
 @Component
