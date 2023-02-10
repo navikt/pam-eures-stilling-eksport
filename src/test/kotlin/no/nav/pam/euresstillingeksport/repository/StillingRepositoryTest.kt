9a7cde91fa16b09acf7682c498ad680329ea1ef5
noraness <nora.storebo.ness@nav.no>
2023-02-10 09:53:06 +0100

    Bruke namedJdbcTemplate for uthenting av stilling i repository.

---
+++ src/test/kotlin/no/nav/pam/euresstillingeksport/repository/StillingRepositoryTest.kt
@@ -0,0 +1,27 @@
+package no.nav.pam.euresstillingeksport.repository
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.springframework.jdbc.core.JdbcTemplate
+import java.util.*
+
+class StillingRepositoryTest {
+
+    lateinit var stillingRepository: StillingRepository
+
+    val jdbcTemplate: JdbcTemplate = mock<JdbcTemplate>(JdbcTemplate::class.java)
+
+    @BeforeEach
+    fun init() {
+        stillingRepository = StillingRepository(jdbcTemplate)
+    }
+
+    @Test
+    fun `findanonnsebyid returnerer null når stilling ikke finnes i DB`() {
+        val randomUuid = UUID.randomUUID().toString()
+        Assertions.assertNull(stillingRepository.findStillingsannonseById(randomUuid))
+    }
+}
