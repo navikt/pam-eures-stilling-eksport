dcfaba452edeb364eb5919e39d4e3341eed456c5
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-08-02 10:18:18 +0200

    Legger til debug-logging for landskoder

--- src/main/kotlin/no/nav/pam/euresstillingeksport/model/GeografiService.kt
+++ src/main/kotlin/no/nav/pam/euresstillingeksport/model/GeografiService.kt
@@ -1,6 +1,7 @@
 package no.nav.pam.euresstillingeksport.model
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.lang.RuntimeException
@@ -14,10 +15,15 @@ import java.net.http.HttpResponse
 class GeografiService(@Autowired private val objectMapper: ObjectMapper) {
     companion object {
         private const val baseUrl = "http://pam-geografi/rest/euland/"
+        private val logger = LoggerFactory.getLogger(GeografiService::class.java)
     }
 
     fun settLandskoder(stilling: Ad): Ad {
+        logger.debug("Setter landskoder for ${stilling.uuid}. LocationList: ${stilling.locationList}")
+
         val nyLocationList = stilling.locationList.map { location ->
+
+
             if (location.landskode != null) return@map location
 
             location.country?.let { hentLandskodeHvisEuresLand(it) }?.let { location.copy(landskode = it.landskode) } ?: location
