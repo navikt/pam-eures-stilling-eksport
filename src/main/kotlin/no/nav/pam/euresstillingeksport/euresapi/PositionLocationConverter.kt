cdb99080a906cd221c73241d8975a568adff26d8
Ken Ove Andersen <ken.ove.andersen@nav.no>
2025-08-27 13:38:17 +0200

    Legg til Eures sine verdier for countrySubDivisionCode fra fylke

--- src/main/kotlin/no/nav/pam/euresstillingeksport/euresapi/PositionLocationConverter.kt
+++ src/main/kotlin/no/nav/pam/euresstillingeksport/euresapi/PositionLocationConverter.kt
@@ -8,7 +8,27 @@ fun Location.toPositionLocation(): PositionLocation {
             cityName = city,
             addressLine = address,
             countryCode = landskode ?: "NO",
-            countrySubDivisionCode = null,
+            countrySubDivisionCode = countyToCountrySubDivisionCode(county),
             postalCode = postalCode
     ))
+}
+
+fun countyToCountrySubDivisionCode(county: String?): String? {
+    return when (county?.uppercase()) {
+        "TROMS" -> "NO072"
+        "FINNMARK" -> "NO073"
+        "OSLO" -> "NO081"
+        "ØSTFOLD" -> "NO083"
+        "AKERSHUS" -> "NO084"
+        "BUSKERUD" -> "NO085"
+        "AGDER" -> "NO092"
+        "VESTFOLD" -> "NO093"
+        "TELEMARK" -> "NO094"
+        "ROGALAND" -> "NO0A1"
+        "VESTLAND" -> "NO0A2"
+        "MØRE OG ROMSDAL" -> "NO0A3"
+        "JAN MAYEN" -> "NO0B1"
+        "SVALBARD" -> "NO0B2"
+        else -> null
+    }
 }
